56338ac2c5f8f0bf19fbdb30b1f12d7e75051bab
mavm0406 <mavm040601@gmail.com>
2024-06-03 08:38:53 +0200

    cambio el fichero de admin

--- src/main/java/views/Admin.java
+++ src/main/java/views/Admin.java
@@ -6,20 +6,16 @@ package views;
 
 /**
  *
- * @author miguel
+ * @author migue
  */
 public class Admin extends javax.swing.JDialog {
 
     /**
      * Creates new form Admin
-     * @param parent
-     * @param modal
      */
-    public Admin(Programa parent, boolean modal) {
+    public Admin(AdminLogin parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        
-        setLocationRelativeTo(parent);
     }
 
     /**
@@ -33,6 +29,10 @@ public class Admin extends javax.swing.JDialog {
 
         jPanel1 = new javax.swing.JPanel();
         jButtonCerrar = new javax.swing.JButton();
+        jButtonCrudProd = new javax.swing.JButton();
+        jButtonConsultarTickets = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -48,21 +48,78 @@ public class Admin extends javax.swing.JDialog {
             }
         });
 
+        jButtonCrudProd.setBackground(new java.awt.Color(255, 153, 153));
+        jButtonCrudProd.setFont(new java.awt.Font("Liberation Sans", 1, 15)); // NOI18N
+        jButtonCrudProd.setForeground(new java.awt.Color(0, 0, 0));
+        jButtonCrudProd.setText("CRUD Productos");
+        jButtonCrudProd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonCrudProdActionPerformed(evt);
+            }
+        });
+
+        jButtonConsultarTickets.setBackground(new java.awt.Color(255, 153, 153));
+        jButtonConsultarTickets.setFont(new java.awt.Font("Liberation Sans", 1, 15)); // NOI18N
+        jButtonConsultarTickets.setForeground(new java.awt.Color(0, 0, 0));
+        jButtonConsultarTickets.setText("Consultar Tickets");
+        jButtonConsultarTickets.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonConsultarTicketsActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(308, Short.MAX_VALUE)
-                .addComponent(jButtonCerrar)
-                .addGap(16, 16, 16))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(78, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButtonCerrar)
+                        .addGap(17, 17, 17))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButtonCrudProd)
+                        .addGap(38, 38, 38)
+                        .addComponent(jButtonConsultarTickets)
+                        .addGap(71, 71, 71))))
         );
+
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButtonConsultarTickets, jButtonCrudProd});
+
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(261, Short.MAX_VALUE)
+                .addContainerGap(35, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonCrudProd)
+                    .addComponent(jButtonConsultarTickets))
+                .addGap(18, 18, 18)
                 .addComponent(jButtonCerrar)
-                .addGap(15, 15, 15))
+                .addGap(14, 14, 14))
+        );
+
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButtonConsultarTickets, jButtonCrudProd});
+
+        jPanel2.setBackground(new java.awt.Color(16, 16, 16));
+
+        jLabel1.setBackground(new java.awt.Color(51, 51, 51));
+        jLabel1.setFont(new java.awt.Font("Liberation Sans", 1, 20)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 153, 153));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Administracion del sistema.");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -70,10 +127,14 @@ public class Admin extends javax.swing.JDialog {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -83,11 +144,23 @@ public class Admin extends javax.swing.JDialog {
         dispose();
     }//GEN-LAST:event_jButtonCerrarActionPerformed
 
+    private void jButtonCrudProdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCrudProdActionPerformed
+        new AdminCrudProductos(this, true);
+    }//GEN-LAST:event_jButtonCrudProdActionPerformed
+
+    private void jButtonConsultarTicketsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonConsultarTicketsActionPerformed
+        new AdminConsultarTickets(this, true);
+    }//GEN-LAST:event_jButtonConsultarTicketsActionPerformed
+
     /**
      * @param args the command line arguments
      */
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonCerrar;
+    private javax.swing.JButton jButtonConsultarTickets;
+    private javax.swing.JButton jButtonCrudProd;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
